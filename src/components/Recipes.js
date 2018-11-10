1f6bcb60fd22fdf4728591270672554ce87635e9
Shayan Sadar <shayansadar37@gmail.com>
2018-11-09 19:20:48 -0600

    Update
    No router used till this stage

--- src/components/Recipes.js
+++ src/components/Recipes.js
@@ -5,9 +5,18 @@ const Recipes = props => (
         <div className="row">
             {props.recipes.map((recipe) =>{
                 return (
-                    <div className="col-md-4">
-                        <div className="recipe__box">
-                            <img src={recipe.img_url} alt={recipe.title}/>
+                    <div key={recipe.recipe_id} className="col-md-4" style={{marginBottom: "2rem"}}>
+                        <div className="recipes__box">
+                            <img className="recipe__box-img"
+                            src={recipe.image_url}
+                            alt={recipe.title}/>
+                            <div className="recipe__text">
+                                <h5 className="recipes__title">
+                                    {recipe.title.length < 20 ? `${recipe.title}` : `${recipe.title.substring(0, 25)}...`}
+                                </h5>
+                                <p className="recipes__subtitle">Publisher: <span>{recipe.publisher}</span></p>
+                            </div>
+                            <button className="recipe_buttons">View</button>
                         </div>
                     </div>
                 );
